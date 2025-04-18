e89ff07830bb0dceed8ee3b84e6d59ed97dea6db
Zulkifal05 <zulkifalali299@gmail.com>
2025-04-18 22:04:30 +0500

    Made till draw javascript function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const boxes = document.querySelectorAll(".boxes");
 const msg = document.querySelector(".msg");
 let turnX = true;
+let winnerFound = false;
+let checkedBoxesCount = 0;
 let winPatterns = 
     [
         [0,1,2],
@@ -17,38 +19,46 @@ boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if (turnX) {
             box.innerText = "X";
-            checkWinner("X");
             msg.innerText = "Move of player O";
+            checkedBoxesCount++;
+            checkWinner("X");
+            checkDraw();
             turnX = false;
         }
         else {
             box.innerText = "O";
-            checkWinner("0");
             msg.innerText = "Move of player X";
+            checkedBoxesCount++;
+            checkWinner("0");
+            checkDraw();
             turnX = true;
         }
         box.disabled = true;
-    });
+    })
 });
 
 //Function for checking winner alongside
 function checkWinner(winner) {
-    for (let box of boxes) {
-        winPatterns.forEach((pattern) => {
-            let val1 = boxes[pattern[0]].innerText;
-            let val2 = boxes[pattern[1]].innerText;
-            let val3 = boxes[pattern[2]].innerText;
+    winPatterns.forEach((pattern) => {
+        let val1 = boxes[pattern[0]].innerText;
+        let val2 = boxes[pattern[1]].innerText;
+        let val3 = boxes[pattern[2]].innerText;
 
-            if (val1 !== "" && val2 !== "" && val3 != "") {
-
-                if (val1 === val2 && val2 === val3) {
-                    msg.innerText = `${winner} has Won!`;
-                    console.log("Winner");
-                }
-                else {
-                    console.log("ABC");
-                }
+        if (val1 !== "" && val2 !== "" && val3 != "") { // Condition for checking empty space
+            if (val1 === val2 && val2 === val3) { // Condition for checking a winner
+                msg.innerText = `${winner} has Won!`;
+                winnerFound = true;
+                boxes.forEach((box) => {
+                    box.disabled = true;
+                })
             }
-        });
+        }
+    })
+}
+
+//Function for checking a draw
+function checkDraw() {
+    if (checkedBoxesCount === 9 && !winnerFound) {
+        msg.innerText = "Game Drawn!";
     }
 }
